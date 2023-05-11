9e8d7bfe98afcd43c365573ecdd62372c4af321e
Benjamin Tuna <benjamintuna@gmail.com>
2023-05-11 11:16:18 +0200

    adjustments

--- src/main/java/com/jiraRestApi/datajiramongodb/DataJiraMongodbApplication.java
+++ src/main/java/com/jiraRestApi/datajiramongodb/DataJiraMongodbApplication.java
@@ -1,14 +1,21 @@
 package com.jiraRestApi.datajiramongodb;
-
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoOperations;
 
 @SpringBootApplication
 public class DataJiraMongodbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataJiraMongodbApplication.class, args);
+	}
+	@Bean
+	public CommandLineRunner initData(MongoOperations mongo) {
+		return (String... args) -> {
+			mongo.dropCollection(JiraIssue.class);
+		};
 	}
 
 }
